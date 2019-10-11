8112419048716cef3318e3f8de6ac3c6eb1eef8f
Aaron Gokani <aarongokani@hotmail.com>
2019-10-11 03:07:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
     if
       (board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X")
       return win_combo
-    elsif 
+    elsif
       (board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O")
       return win_combo
     else
